f26eba10077bac99ba8c30ef3db89f8edc963139
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-10-15 21:50:33 +0500

    text to voice

---
+++ challenges/text_to_voice.py
@@ -0,0 +1,7 @@
+from os import system
+
+namestoShouout = ["Muhammad Hashim" , "Muhammad Dayan" , "Muhammad Ali" , "Muhammad Sabir" , "Muhammad Hayan"]
+
+
+for name in namestoShouout:
+    system(f'say Shoutout to {name}')
